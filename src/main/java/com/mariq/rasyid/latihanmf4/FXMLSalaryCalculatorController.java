bedd43a6210b9805ec90229bf3d597614170ed16
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-11-11 13:12:18 +0700

    Added FXMLSalaryCalculatorController and modifed FXMLSALARY CALCULATOR

--- src/main/java/com/mariq/rasyid/latihanmf4/FXMLSalaryCalculatorController.java
+++ src/main/java/com/mariq/rasyid/latihanmf4/FXMLSalaryCalculatorController.java
@@ -1,12 +1,19 @@
+package com.mariq.rasyid.latihanmf4;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mariq.rasyid.latihanmf4;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +22,19 @@ import javafx.fxml.Initializable;
  */
 public class FXMLSalaryCalculatorController implements Initializable {
 
+    @FXML
+    private TabPane salarycalculator;
+    @FXML
+    private TextField txt_name;
+    @FXML
+    private ComboBox<?> chb_rank;
+    @FXML
+    private TextField txtday;
+    @FXML
+    private Button calculate_btn;
+    @FXML
+    private Button btn_reset;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +42,13 @@ public class FXMLSalaryCalculatorController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void calculate_click(ActionEvent event) {
+    }
+
+    @FXML
+    private void reset_click(ActionEvent event) {
+    }
     
 }
